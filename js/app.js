bd4f687d0f27c827e4b7c98708197bbd673c1f02
Chahid_R <chahid.rhouane@itsv.at>
2025-06-04 15:13:06 +0200

    Bild Anschau änderungen

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+const images = new Map();
 
 let image1 = document.getElementById("1");
 let image2 = document.getElementById("2");
@@ -9,7 +9,7 @@ let image6 = document.getElementById("6");
 let image7 = document.getElementById("7");
 let image8 = document.getElementById("8");
 
-const images = [
+const imagesStyle = [
   image1,
   image2,
   image3,
@@ -20,15 +20,64 @@ const images = [
   image8
 ];
 
+images.set(image1,"Stierblut");
+images.set(image2,"Heimat");
+images.set(image3,"Die tanzenden Spermien");
+images.set(image4,"Jungle");
+images.set(image5,"Skyline");
+images.set(image6,"Tropical_garden");
+images.set(image7,"Summer 99 Lucky Strike Limited Edition");
+images.set(image8,"Diversity");
+
+
+
 let text = document.getElementById("text")
 
-for (let i = 0; i < images.length; i++){
-  let image = images.at(i);
+for (const [key, value] of images){
+  key.addEventListener("mouseenter", () =>{
 
-  image.addEventListener("mouseenter", () => {
-    text.innerText = image.src;
+
+    text.innerHTML = value;
+
+    for (let i = 0; i < imagesStyle.length;i++){
+      imagesStyle.at(i).style.opacity = "0.5";
+    }
+    switch (key){
+      case image1:
+        image1.style.opacity = "1.0";
+        break;
+      case image2:
+        image2.style.opacity = "1.0";
+        break;
+      case image3:
+        image3.style.opacity = "1.0";
+        break;
+      case image4:
+        image4.style.opacity = "1.0";
+        break;
+      case image5:
+        image5.style.opacity = "1.0";
+        break;
+      case image6:
+        image6.style.opacity = "1.0";
+        break;
+      case image7:
+        image7.style.opacity = "1.0";
+        break;
+      case image8:
+        image8.style.opacity = "1.0";
+        break;
+
+    }
   })
 
+  key.addEventListener("mouseleave", () =>{
+    text.innerHTML = "";
+    for (let i = 0; i < imagesStyle.length;i++){
+      imagesStyle.at(i).style.opacity = "1.0";
+    }
+
+  })
 }
 
 
--- js/index5JS.js
+++ js/index5JS.js
@@ -1,40 +1,83 @@
-const images = [
-  "img/Index5/B-IN-Frau_sein-Ton.jpg",
-  "img/Index5/B-IN-Frau_sein-Karton.jpg",
-  "img/Index5/B-IN-Puppenspiel.jpg",
-  "img/Index5/B-IN-Mein_Lebens-Baum-Traum.jpg",
-  "img/Index5/B-IN-bewegt.jpg",
-  "img/Index5/B-IN-die_Kraft_in_mir0.jpg",
-  "img/Index5/B-IN-orange_Lebensglueck.jpg",
-  "img/Index5/B-IN-uebermuetig_durch_die_Luefte0.jpg"
-]
+const images = new Map();
 
-let currentIndex = 0;
-const nextButton = document.getElementById("nextBtn");
-const prevButton = document.getElementById("prevBtn");
-let currentimage = document.getElementById("sliderImage");
+let image1 = document.getElementById("1");
+let image2 = document.getElementById("2");
+let image3 = document.getElementById("3");
+let image4 = document.getElementById("4");
+let image5 = document.getElementById("5")
+let image6 = document.getElementById("6");
+let image7 = document.getElementById("7");
+let image8 = document.getElementById("8");
+
+const imagesStyle = [
+  image1,
+ image2,
+ image3,
+image4,
+image5,
+ image6,
+ image7,
+ image8
+];
+
+images.set(image1,"Frau sein Ton");
+images.set(image2,"Frau sein Karton");
+images.set(image3,"Puppenspiel");
+images.set(image4,"Mein Lebens Baum Traum");
+images.set(image5,"Bewegt");
+images.set(image6,"Die Kraft in mir");
+images.set(image7,"Orange Lebensglueck");
+images.set(image8,"Übermuetig durch die Lüfte");
 
 
-nextButton.addEventListener("click", () => {
-  if (currentIndex >= images.length - 1){
-    currentIndex = images.length - 1;
-    showImage(currentIndex);
-  }else{
-    currentIndex++;
-    showImage(currentIndex);
-  }
-});
+let text = document.getElementById("text")
 
-prevButton.addEventListener("click", () => {
-  if (currentIndex <= 0){
-    currentIndex = 0;
-    showImage(currentIndex);
-  }else{
-    currentIndex--;
-    showImage(currentIndex);
-  }
-})
+for (const [key, value] of images){
+  key.addEventListener("mouseenter", () =>{
 
-function showImage(Index){
-  currentimage.src = images.at(Index);
+
+    text.innerHTML = value;
+
+    for (let i = 0; i < imagesStyle.length;i++){
+      imagesStyle.at(i).style.opacity = "0.5";
+    }
+    switch (key){
+      case image1:
+        image1.style.opacity = "1.0";
+        break;
+      case image2:
+        image2.style.opacity = "1.0";
+        break;
+      case image3:
+        image3.style.opacity = "1.0";
+        break;
+      case image4:
+        image4.style.opacity = "1.0";
+        break;
+        case image5:
+          image5.style.opacity = "1.0";
+        break;
+      case image6:
+        image6.style.opacity = "1.0";
+        break;
+      case image7:
+        image7.style.opacity = "1.0";
+        break;
+      case image8:
+        image8.style.opacity = "1.0";
+        break;
+
+    }
+  })
+
+  key.addEventListener("mouseleave", () =>{
+    text.innerHTML = "";
+    for (let i = 0; i < imagesStyle.length;i++){
+      imagesStyle.at(i).style.opacity = "1.0";
+    }
+
+  })
 }
+
+
+
